f8f5bdcbfe3057d5625c2dd0d04b5fcd4522ea13
chamow97 <jchandramowli15039@it.ssn.edu.in>
2018-04-04 08:40:55 +0530

    Add Notification App

---
+++ Notification/app/src/main/java/com/example/root/notification/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.root.notification;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendNotification(View view){
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("New Quote")
+                .setContentText("Stay Hungry! Stay Foolish!");
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(001, mBuilder.build());
+
+    }
+}
